49620468b11c8cdb3fd022119ac43f89402577a9
Umang Sharma <umng01@gmail.com>
2021-10-24 20:37:51 +0530

    Weather! Oh, la la!

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
+  hours = hours % 12;
+  hours = `0${hours}`;
+  
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
-  }
-
+  } 
   let days = [
     "Sunday",
     "Monday",
